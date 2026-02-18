649f65ea3ac89cfdd87b6e4066fc488867d05a53
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-02-18 03:26:13 +0100

    Add redirectWithAlert and -Toast modules

---
+++ app/alert.server.ts
@@ -0,0 +1,117 @@
+import { createCookieSessionStorage, redirect } from "react-router";
+import { sanitizeUserHtml } from "./utils/.server/sanitize";
+import { z, ZodError } from "zod";
+import { combineHeaders } from "./utils/.server/headers";
+
+type AlertLevel = "neutral" | "positive" | "attention" | "negative";
+
+type Alert = {
+  level?: AlertLevel;
+  message: string;
+  isRichtext?: boolean;
+};
+
+const alertLevels: AlertLevel[] = ["positive", "attention", "negative"];
+
+const alertSchema = z.object({
+  message: z.string(),
+  level: z
+    .string()
+    .refine((level) => alertLevels.includes(level as AlertLevel), {
+      message: "Invalid alert level",
+    })
+    .optional(),
+  isRichtext: z.boolean().optional(),
+});
+
+const AlertKey = "alert";
+
+const alertSessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "alert",
+    sameSite: "lax",
+    path: "/",
+    httpOnly: true,
+    // TODO: Use environment variable for the secrets, when env.server.ts and ENV is ready
+    secrets: ["s1mpl3s3cr3t3n0ugh4d3v"],
+    // process.env.NODE_ENV === "production"
+    //   ? process.env.SESSION_SECRET.split(",")
+    //   : ["secret"],
+    secure: process.env.NODE_ENV === "production",
+  },
+});
+
+export async function redirectWithAlert(
+  url: string,
+  alert: Alert,
+  init?: ResponseInit
+) {
+  return redirect(url, {
+    ...init,
+    headers: combineHeaders(
+      init?.headers,
+      await createAlertHeaders(alert.message, alert.level, alert.isRichtext)
+    ),
+  });
+}
+
+async function createAlertHeaders(
+  message: string,
+  level?: AlertLevel,
+  isRichtext?: boolean
+) {
+  const session = await alertSessionStorage.getSession();
+  const alert = {
+    message,
+    level: level ?? "positive",
+    isRichtext: isRichtext ?? false,
+  };
+  session.flash(AlertKey, alert);
+  const cookie = await alertSessionStorage.commitSession(session);
+  return new Headers({ "set-cookie": cookie });
+}
+
+export async function getAlert(request: Request) {
+  const session = await alertSessionStorage.getSession(
+    request.headers.get("cookie")
+  );
+  const alert = session.get(AlertKey);
+  // Early return when cookie session is not set
+  if (alert === undefined) {
+    return {
+      alert: null,
+      headers: null,
+    };
+  }
+  const sanitizedAlert = {
+    ...alert,
+    message: sanitizeUserHtml(alert.message),
+  };
+  // Parse data against the schema
+  try {
+    alertSchema.parse(sanitizedAlert);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      console.error(
+        "Validation errors while parsing the alert cookie session:",
+        error.message
+      );
+    } else {
+      console.error(
+        "Unexpected error during parsing the alert cookie session:",
+        error
+      );
+    }
+    console.error("The alert data was not returned.");
+    return {
+      alert: null,
+      headers: null,
+    };
+  }
+  return {
+    alert: sanitizedAlert as Alert,
+    headers: new Headers({
+      "set-cookie": await alertSessionStorage.destroySession(session),
+    }),
+  };
+}
---
+++ app/utils/.server/sanitize.ts
@@ -0,0 +1,67 @@
+import sanitizeHtml from "sanitize-html";
+
+const allowedTags = [
+  "b",
+  "i",
+  "em",
+  "strong",
+  "a",
+  "ul",
+  "ol",
+  "p",
+  "span",
+  "li",
+  "br",
+];
+
+const allowedAttributes = {
+  a: [
+    "class",
+    "href",
+    {
+      name: "rel",
+      multiple: true,
+      values: ["noopener", "noreferrer"],
+    },
+    {
+      name: "target",
+      values: ["_blank"],
+    },
+  ],
+  b: ["class"],
+  i: ["class"],
+  em: ["class"],
+  strong: ["class"],
+  ul: ["class"],
+  ol: ["class"],
+  p: ["class"],
+  span: ["class"],
+  li: ["class"],
+  br: ["class"],
+};
+
+export const sanitizeUserHtml = (
+  html: string | null,
+  options?: {
+    allowedTags?: string[];
+    allowedAttributes?: { [key: string]: string[] };
+  }
+) => {
+  if (html === null) {
+    return null;
+  }
+  if (allowedTags === undefined) {
+    return null;
+  }
+  if (allowedAttributes === undefined) {
+    return null;
+  }
+  const sanitizedHtml = sanitizeHtml(
+    html,
+    options ?? {
+      allowedTags,
+      allowedAttributes,
+    }
+  ).replaceAll("<br />", "<br>");
+  return sanitizedHtml;
+};
