2155a147fd2ad6845736f6c7c85f6e4ac50d8fd2
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-02-23 20:10:06 +0100

    fix: be explicit which routes to prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,14 @@
 import type { Config } from "@react-router/dev/config";
 
+const staticRoutes = ["/"];
+
+if (process.env.NODE_ENV !== "production") {
+  staticRoutes.push("/playground");
+}
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  prerender: true,
+  prerender: staticRoutes,
 } satisfies Config;
