e1cf2f17bc1de9a98565fc8c3a7983e505607f5d
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-03-01 20:22:37 +0100

    Remove prerender as its app specific and not starter related

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,14 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
-const staticRoutes = ["/"];
-
-if (process.env.NODE_ENV !== "production") {
-  staticRoutes.push("/playground");
-}
-
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  prerender: staticRoutes,
 } satisfies Config;
