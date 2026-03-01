9cc0f59866b917a82db6bd4a6f38bdcf3ce3ec14
nac62116 <56918308+nac62116@users.noreply.github.com>
2026-03-01 13:40:23 +0100

    Optimize nginx to use a shared template default.conf for both dev and prod and provide variables via envsubst

--- Makefile
+++ Makefile
@@ -8,11 +8,13 @@ help:
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
 create-dev-certs: ## Create self-signed development certificates (for HTTPS in development, to test docker setup locally). Note that browsers will show a warning when using self-signed certificates, which is expected. You can usually click "Advanced" and then "Proceed to localhost (unsafe)" to bypass the warning when testing locally.
-	@mkdir -p ./nginx/dev-certs
-	openssl req -x509 -newkey rsa:2048 -keyout ./nginx/dev-certs/localhost.key -out ./nginx/dev-certs/localhost.crt -days 365 -nodes -subj "/CN=localhost"
-	openssl dhparam -out ./nginx/dev-certs/ssl-dhparams.pem 2048
-	curl -o ./nginx/certbot-files-for-dev/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/refs/heads/main/certbot-nginx/src/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
-	curl -o ./nginx/certbot-files-for-dev/LICENSE.txt https://raw.githubusercontent.com/certbot/certbot/refs/heads/main/LICENSE.txt
+	@mkdir -p ./nginx/dev/ssl
+	@mkdir -p ./nginx/dev/ssl/certs
+	openssl req -x509 -newkey rsa:2048 -keyout ./nginx/dev/ssl/certs/privkey.pem -out ./nginx/dev/ssl/certs/fullchain.pem -days 365 -nodes -subj "/CN=localhost"
+	openssl dhparam -out ./nginx/dev/ssl/certs/ssl-dhparams.pem 2048
+	@mkdir -p ./nginx/dev/ssl/certbot-files
+	curl -o ./nginx/dev/ssl/certbot-files/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/refs/heads/main/certbot-nginx/src/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+	curl -o ./nginx/dev/ssl/certbot-files/LICENSE.txt https://raw.githubusercontent.com/certbot/certbot/refs/heads/main/LICENSE.txt
 
 setup-certbot-with-autorenewal: ## Make sure to add a correct A record for your domain before you run this. This sets up certbot with nginx plugin and auto-renewal (for production, to get real certificates).
 	@sudo apt update
--- app/lib/utils/env.server.ts
+++ app/lib/utils/env.server.ts
@@ -5,57 +5,67 @@ import { z } from "zod";
 // - Server: process.env.<ENV_VARIABLE>
 // - Client: window.ENV.<ENV_VARIABLE> or ENV.<ENV_VARIABLE>
 
-const schema = z.object({
-  NODE_ENV: z.enum(["production", "development", "test"] as const),
-  BASE_URL: z.string().min(1),
-  DOMAIN: z.string().min(1),
-  SYSTEM_MAIL_SENDER: z.string().min(1),
-  MAILER_HOST: z.string().min(1),
-  MAILER_PORT: z
-    .string()
-    .min(1)
-    .transform((val) => parseInt(val, 10)),
-  MAILER_USER: z
-    .string()
-    .optional()
-    .transform((val) => (val === "" ? undefined : val)),
-  MAILER_PASS: z
-    .string()
-    .optional()
-    .transform((val) => (val === "" ? undefined : val)),
-  ALLOW_INDEXING: z
-    .string()
-    .optional()
-    .transform((val) => {
-      if (typeof val === "undefined") return true;
-      if (val === "") return true;
-      return val === "true";
-    }),
-  MATOMO_URL: z
-    .string()
-    .optional()
-    .transform((val) => (val === "" ? undefined : val)),
-  MATOMO_SITE_ID: z
-    .string()
-    .optional()
-    .transform((val) => (val === "" ? undefined : val)),
-  SESSION_SECRETS: z
-    .string()
-    .min(1)
-    .transform((val) => val.split(",")),
-  HONEYPOT_SECRETS: z
-    .string()
-    .min(1)
-    .transform((val) => val.split(",")),
-  CSRF_SECRETS: z
-    .string()
-    .min(1)
-    .transform((val) => val.split(",")),
-  MESSAGE_SECRETS: z
-    .string()
-    .min(1)
-    .transform((val) => val.split(",")),
-});
+const schema = z
+  .object({
+    NODE_ENV: z.enum(["production", "development", "test"] as const),
+    BASE_URL: z.string().optional(),
+    SYSTEM_MAIL_SENDER: z.string().min(1),
+    MAILER_HOST: z.string().min(1),
+    MAILER_PORT: z
+      .string()
+      .min(1)
+      .transform((val) => parseInt(val, 10)),
+    MAILER_USER: z
+      .string()
+      .optional()
+      .transform((val) => (val === "" ? undefined : val)),
+    MAILER_PASS: z
+      .string()
+      .optional()
+      .transform((val) => (val === "" ? undefined : val)),
+    ALLOW_INDEXING: z
+      .string()
+      .optional()
+      .transform((val) => {
+        if (typeof val === "undefined") return true;
+        if (val === "") return true;
+        return val === "true";
+      }),
+    MATOMO_URL: z
+      .string()
+      .optional()
+      .transform((val) => (val === "" ? undefined : val)),
+    MATOMO_SITE_ID: z
+      .string()
+      .optional()
+      .transform((val) => (val === "" ? undefined : val)),
+    SESSION_SECRETS: z
+      .string()
+      .min(1)
+      .transform((val) => val.split(",")),
+    HONEYPOT_SECRETS: z
+      .string()
+      .min(1)
+      .transform((val) => val.split(",")),
+    CSRF_SECRETS: z
+      .string()
+      .min(1)
+      .transform((val) => val.split(",")),
+    MESSAGE_SECRETS: z
+      .string()
+      .min(1)
+      .transform((val) => val.split(",")),
+  })
+  .transform((env) => {
+    if (typeof env.BASE_URL === "undefined" || env.BASE_URL === "") {
+      if (env.NODE_ENV === "production") {
+        env.BASE_URL = "https://localhost";
+      } else {
+        env.BASE_URL = "http://localhost:3000";
+      }
+    }
+    return env;
+  });
 
 declare global {
   namespace NodeJS {
